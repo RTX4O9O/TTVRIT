cf2384477f4148249c386b387b10d189d633dc73
RTX <jaykuo1203@gmail.com>
2024-05-02 01:15:44 +0800

    add frame (APPWindow.java)

---
+++ src/APPWindow.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class APPWindow extends JFrame {
+    APPWindow() {
+        this.setTitle("Taiwan Traffic Violation Reporting Integration Tool (TTVRIT) <alpha>");
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setLayout(new FlowLayout());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        this.pack();
+        this.setVisible(true);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,8 @@ import ReportPage.ReportPageHSH;
 
 public class Main {
     public static void main(String[] args) {
-        ReportPageHSH page = new ReportPageHSH();
+        APPWindow appwindow = new APPWindow();
+
 
     }
 }
