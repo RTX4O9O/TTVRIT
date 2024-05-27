5683cf243537b2c251d5c6ba118c8a84c41f8c27
rtx <jaykuo1203@gmail.com>
2024-05-27 13:38:45 +0800

    add Notify.java

---
+++ src/Notify.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Notify {
+
+    public void error() {
+        JOptionPane.showMessageDialog(null, "An error occurred", "Error", JOptionPane.ERROR_MESSAGE);
+    }
+    public void warning() {
+
+    }
+}
