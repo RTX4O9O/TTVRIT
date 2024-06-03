392e500302088fd5e488f071ba255948e905ae6c
rtx <s211181@nehs.hc.edu.tw>
2024-06-03 14:58:21 +0800

    add HistoryPage

---
+++ src/main/java/me/rtx4090/gui/tabs/history/HistoryPage.java
@@ -0,0 +1,11 @@
+package me.rtx4090.gui.tabs.history;
+
+import javax.swing.*;
+
+public class HistoryPage {
+    public JPanel historyPanel = new JPanel();
+
+    public HistoryPage() {
+        historyPanel.setLayout(null);
+    }
+}
