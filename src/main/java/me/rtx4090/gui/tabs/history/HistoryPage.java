5a76642efa3a36845e9b2dfa3f6628400d81b97c
RTX <jaykuo1203@gmail.com>
2024-06-05 14:03:30 +0800

    change HistoryPage.java

--- src/main/java/me/rtx4090/gui/tabs/history/HistoryPage.java
+++ src/main/java/me/rtx4090/gui/tabs/history/HistoryPage.java
@@ -1,11 +1,29 @@
 package me.rtx4090.gui.tabs.history;
 
 import javax.swing.*;
+import java.io.File;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class HistoryPage {
     public JPanel historyPanel = new JPanel();
-
+    File historyFolder = new File(System.getProperty("user.home") + "/TTVRIT/Reports");
     public HistoryPage() {
         historyPanel.setLayout(null);
+        // if history folder exists
+        if (!historyFolder.exists()) {
+            historyFolder.mkdirs();
+        }
+        File[] filesInsideHistoryFolder = historyFolder.listFiles();
+        ArrayList<String[]> histories = new ArrayList<>();
+        for (File file : filesInsideHistoryFolder) {
+            if (file.isDirectory()) {
+                File propertiesFile = new File(file.getAbsolutePath() + "/properties.json");
+                String[] reportProperties = new String[3];
+
+            }
+        }
+
+
     }
 }
