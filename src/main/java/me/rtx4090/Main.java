a0eb9f994e899f649f1b451d704a7d7c2aa6e6ef
RTX <jaykuo1203@gmail.com>
2024-06-09 17:09:16 +0800

    sorting

--- src/main/java/me/rtx4090/Main.java
+++ src/main/java/me/rtx4090/Main.java
@@ -1,12 +1,11 @@
 package me.rtx4090;
 
 import me.rtx4090.gui.MainWindow;
+import me.rtx4090.gui.tabs.report.Driver;
 
 public class Main {
     public static void main(String[] args) {
         MainWindow appwindow = new MainWindow();
         //Notify.error();
-
-        ;
     }
 }
