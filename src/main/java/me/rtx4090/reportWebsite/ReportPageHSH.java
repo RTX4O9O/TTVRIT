3a290c578b258751104ed159996a4033b8753eb8
RTX <jaykuo1203@gmail.com>
2024-06-09 22:02:35 +0800

    define all elements in HSH report site

--- src/main/java/me/rtx4090/reportWebsite/ReportPageHSH.java
+++ src/main/java/me/rtx4090/reportWebsite/ReportPageHSH.java
@@ -17,12 +17,25 @@ public class ReportPageHSH {
         WebElement reporterPhone = driver.findElement(By.id("tel"));
         WebElement reporterAddress = driver.findElement(By.id("address2"));
 
-
-
         Select caseDate = new Select(driver.findElement(By.id("select2-report_date-container")));
-        Select caseHour = new Select(driver.findElement(By.id("select2-report_month")));
+        Select caseHour = new Select(driver.findElement(By.id("select2-hour-container")));
+        Select caseMinute = new Select(driver.findElement(By.id("select2-minute-container")));
+
+        WebElement licensePlateNum = driver.findElement(By.id("carcode"));
+
+        Select city = new Select(driver.findElement(By.id("select2-cakeslt1-container")));
+        Select road = new Select(driver.findElement(By.id("select2-cakeslt2-container")));
+        WebElement no = driver.findElement(By.id("address"));
+
+        Select reason = new Select(driver.findElement(By.cssSelector("select[name=legislation2]")));
+
+        WebElement fileInput1 = driver.findElement(By.xpath("(//input[@id='data[]'])[1]"));
+        WebElement fileInput2 = driver.findElement(By.xpath("(//input[@id='data[]'])[2]"));
+        WebElement fileInput3 = driver.findElement(By.xpath("(//input[@id='data[]'])[3]"));
+        WebElement fileInput4 = driver.findElement(By.xpath("(//input[@id='data[]'])[4]"));
 
         WebElement captcha = driver.findElement(By.id("checknum"));
+        WebElement captchaImg = driver.findElement(By.className("captcha_img"));
         WebElement submitButton = driver.findElement(By.id("submit"));
     }
 
