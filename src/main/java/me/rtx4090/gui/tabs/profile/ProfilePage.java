a1743ca9af1d1064d2cd6972483d5b4c3a630daf
yw <ryanpipi13@gmail.com>
2024-06-10 13:21:41 +0800

    排版

--- src/main/java/me/rtx4090/gui/tabs/profile/ProfilePage.java
+++ src/main/java/me/rtx4090/gui/tabs/profile/ProfilePage.java
@@ -4,12 +4,14 @@ import com.google.gson.Gson;
 import me.rtx4090.Profile;
 import me.rtx4090.Profiles;
 import me.rtx4090.gui.Notify;
+import me.rtx4090.gui.*;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.math.RoundingMode;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Map;
@@ -26,7 +28,6 @@ public class ProfilePage implements ActionListener{
     JLabel hintLabel;
     JButton createProfileButton;
     JPanel existedProfilesPanel;
-
     public ProfilePage() {
 
         profilePanel.setLayout(new GridLayout(1, 2, 0, 0));
@@ -42,79 +43,119 @@ public class ProfilePage implements ActionListener{
             JPanel personalInfoInput = new JPanel();
             {
                 personalInfoInput.setLayout(new GridBagLayout());
-
-                JPanel labelsPanel = new JPanel();
-                labelsPanel.setLayout(new GridBagLayout());
-                JPanel fieldsPanel = new JPanel();
-                fieldsPanel.setLayout(new GridBagLayout());
+                personalInfoInput.setBackground(new Color(225,235,241));
+                //JPanel labelsPanel = new JPanel();
+                //labelsPanel.setLayout(new FlowLayout());
+                //JPanel fieldsPanel = new JPanel();
+                //fieldsPanel.setLayout(new GridBagLayout());
 
                 JLabel profileNicknameLabel = new JLabel("檔案名稱");
+                setsizelable(profileNicknameLabel);
                 JLabel nameLabel = new JLabel("姓名");
+                setsizelable(nameLabel);
                 JLabel idLabel = new JLabel("身分證字號");
+                setsizelable(idLabel);
                 JLabel emailLabel = new JLabel("電子郵件");
+                setsizelable(emailLabel);
                 JLabel phoneLabel = new JLabel("電話號碼");
-                JLabel addressLabel = new JLabel("聯絡地址");
+                setsizelable(phoneLabel);
+                JLabel addressLabel = new JLabel("地址");
+                setsizelable(addressLabel);
 
                 profileNicknameField = new JTextField();
+                setextfield(profileNicknameField);
                 nameField = new JTextField();
+                setextfield(nameField);
                 idField = new JTextField();
+                setextfield(idField);
                 emailField = new JTextField();
+                setextfield(emailField);
                 phoneField = new JTextField();
+                setextfield(phoneField);
                 addressField = new JTextField();
+                setextfield(addressField);
+                /////////////////////////////////////////////////
+                profileNicknameLabel.setHorizontalAlignment(SwingConstants.LEFT);
+                nameLabel.setHorizontalAlignment(SwingConstants.LEFT);
+                idLabel.setHorizontalAlignment(SwingConstants.LEFT);
+                emailLabel.setHorizontalAlignment(SwingConstants.LEFT);
+                phoneLabel.setHorizontalAlignment(SwingConstants.LEFT);
+                addressLabel.setHorizontalAlignment(SwingConstants.LEFT);
+
+                /////////////////////////////////////////
 
                 gbc.weighty = 0.2;
                 gbc.fill = GridBagConstraints.BOTH;
-                gbc.gridheight = 1;
-                gbc.gridwidth = 1;
+                //gbc.gridheight = 1;
+                //gbc.gridwidth = 1;
 
-                gbc.gridx = 0;
-                gbc.gridy = 0;
-                gbc.weightx = 0.3; // Set weightx to 0.3 for labels
-                labelsPanel.add(profileNicknameLabel, gbc);
-                gbc.gridy = 1;
-                labelsPanel.add(nameLabel, gbc);
-                gbc.gridy = 2;
-                labelsPanel.add(idLabel, gbc);
-                gbc.gridy = 3;
-                labelsPanel.add(emailLabel, gbc);
-                gbc.gridy = 4;
-                labelsPanel.add(phoneLabel, gbc);
-                gbc.gridy = 5;
-                labelsPanel.add(addressLabel, gbc);
+                //////////////////
+                gbc.weighty = 0.2;
+                gbc.fill = GridBagConstraints.BOTH;
+                //gbc.gridheight = 1;
+                //gbc.gridwidth = 1;
+                gbc.insets = new Insets(0,0,5,2);
+                //////////////////
 
-                gbc.gridy = 0;
-                gbc.weightx = 0.7; // Set weightx to 0.7 for fields
-                fieldsPanel.add(profileNicknameField, gbc);
-                gbc.gridy = 1;
-                fieldsPanel.add(nameField, gbc);
-                gbc.gridy = 2;
-                fieldsPanel.add(idField, gbc);
-                gbc.gridy = 3;
-                fieldsPanel.add(emailField, gbc);
-                gbc.gridy = 4;
-                fieldsPanel.add(phoneField, gbc);
-                gbc.gridy = 5;
-                fieldsPanel.add(addressField, gbc);
+                gbc.gridx=0;
+                gbc.gridy=0;
+                gbc.weightx=0.1;
+                personalInfoInput.add(profileNicknameLabel,gbc);
 
-                gbc.gridx = 0;
-                gbc.gridy = 0;
-                gbc.weightx = 0.3; // Set weightx to 0.3 for labelsPanel
-                personalInfoInput.add(labelsPanel, gbc);
-                gbc.gridx = 1;
-                gbc.weightx = 0.7; // Set weightx to 0.7 for fieldsPanel
-                personalInfoInput.add(fieldsPanel, gbc);
+                gbc.gridx=1;
+                gbc.weightx=0.9;
+                personalInfoInput.add(profileNicknameField,gbc);
+///////////////////////////////////////////////////////////////////
+
+                gbc.gridx=0;
+                gbc.gridy=1;
+                personalInfoInput.add(nameLabel,gbc);
+
+                gbc.gridx=1;
+                personalInfoInput.add(nameField,gbc);
+                /////////////////////////////////////////
+                gbc.gridx=0;
+                gbc.gridy=2;
+                personalInfoInput.add(idLabel,gbc);
+
+                gbc.gridx=1;
+                personalInfoInput.add(idField,gbc);
+
+                ////////////////////////////////////////////
+                gbc.gridx=0;
+                gbc.gridy=3;
+                personalInfoInput.add(emailLabel,gbc);
+
+                gbc.gridx=1;
+                personalInfoInput.add(emailField,gbc);
+                ////////////////////////////
+                gbc.gridwidth=1;
+                gbc.gridx=0;
+                gbc.gridy=4;
+                personalInfoInput.add(phoneLabel,gbc);
+
+                gbc.gridx=1;
+                personalInfoInput.add(phoneField,gbc);
+                ////////////////////////////////////////////
+                gbc.gridx=0;
+                gbc.gridy=5;
+                personalInfoInput.add(addressLabel,gbc);
+
+                gbc.gridx=1;
+                personalInfoInput.add(addressField,gbc);
             }
-
+            //提示介面與新增按鈕
             hintLabel = new JLabel();
             hintLabel.setVisible(false);
             hintLabel.setHorizontalAlignment(JLabel.CENTER);
-
-            createProfileButton = new JButton("新增");
+            createProfileButton = new JButton("新增檔案");
             createProfileButton.addActionListener(this);
-
+            createProfileButton.setBackground(new Color(0,0,255));
+            createProfileButton.setFont(new Font("SimSun", Font.PLAIN, 18));
+            createProfileButton.setForeground(new Color(225,235,241));
             //button blank
             JPanel buttonLeftBlankPanel = new JPanel();
-
+            buttonLeftBlankPanel.setBackground(new Color(225,235,241));
 
             gbc.gridx = 0;
             gbc.gridy = 0;
@@ -141,9 +182,11 @@ public class ProfilePage implements ActionListener{
 
         }
         createProfiles.setBorder(BorderFactory.createLineBorder(Color.white, 5));
+        createProfiles.setBackground(new Color(225,235,241));
 
 
-        //right part
+
+        //right part/////////////////////////////////////////////////////////
         JPanel existedProfiles = new JPanel();
         existedProfiles.setBackground(Color.red);
         existedProfiles.setLayout(new FlowLayout());
@@ -309,16 +352,18 @@ public class ProfilePage implements ActionListener{
     boolean allTextfieldFilled() {
         return !nameField.getText().isEmpty() && !idField.getText().isEmpty() && !emailField.getText().isEmpty() && !phoneField.getText().isEmpty() && !addressField.getText().isEmpty();
     }
+
     @Override
     public void actionPerformed(ActionEvent e) {
         //create profile
         if (e.getSource() == createProfileButton) {
-
+            hintLabel.setFont(new Font("SimSun", Font.PLAIN, 12));
             if (allTextfieldFilled()) {
 
                 Profile profile = new Profile(nameField.getText(), idField.getText(), emailField.getText(), phoneField.getText(), addressField.getText());
                 addProfile(profile, profileNicknameField.getText());
                 hintLabel.setText("新增成功");
+
                 hintLabel.setVisible(true);
                 nameField.setText("");
                 idField.setText("");
@@ -345,6 +390,27 @@ public class ProfilePage implements ActionListener{
                 }
             }
         }
+    }
+
+    ////////////////////////////////////////////////////////////////////
+    RoundBorder roundBorder = new RoundBorder(15,new Color(37, 150, 190));
+    private  void setsizelable(JLabel label){
+        FontMetrics metrics = label.getFontMetrics(label.getFont());
+        int width = metrics.stringWidth(label.getText());
+        int height = metrics.getHeight();
+        label.setPreferredSize(new Dimension(width, height));
+        label.setForeground(new Color(170,106,255));
+        label.setBackground(new Color(225,235,241));
+        label.setOpaque(true);
+        label.setBorder(roundBorder);
+        label.setFont(new Font("SimSun", Font.PLAIN, 15));
+    }
+
+    private  void setextfield(JTextField textField) {
+        textField.setBorder(roundBorder);
+        textField.setForeground(new Color(170,106,255));
+        textField.setFont(new Font("Dialog", Font.PLAIN, 18));
+        textField.setBackground(new Color(225,235,241));
     }
 }
 
