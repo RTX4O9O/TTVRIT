06aeae2a3cc76ebcbf9c841dfe28d3d6b937bfca
RTX <jaykuo1203@gmail.com>
2024-06-12 12:43:57 +0800

    fix wrong package

--- src/main/java/me/rtx4090/api/TPH.java
+++ src/main/java/me/rtx4090/api/TPH.java
@@ -1,15 +1,9 @@
-package me.rtx4090.reportWebsite;
-
-import org.openqa.selenium.WebDriver;
+package me.rtx4090.api;
 
 public class TPH extends Catalog {
-    @Override
-    public void getElement() {
-
-    }
 
     @Override
-    public void verify() {
+    public void send(String name, String idcard, String tel, String email, String address2, String report_date, String hour, String minute, String carcode, String district, String road_id, String address, String legislation2, String data1, String data2, String data3, String data4) {
 
     }
 }
