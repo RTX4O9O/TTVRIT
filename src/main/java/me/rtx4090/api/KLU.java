58b2dfec53061711df88e742db6511f50f442022
RTX <jaykuo1203@gmail.com>
2024-06-12 12:38:17 +0800

    change report submitting method remove reportWebsite and its content rename package API to api add all classes for api

---
+++ src/main/java/me/rtx4090/api/KLU.java
@@ -0,0 +1,9 @@
+package me.rtx4090.api;
+
+public class KLU extends Catalog {
+
+    @Override
+    public void send(String name, String idcard, String tel, String email, String address2, String report_date, String hour, String minute, String carcode, String district, String road_id, String address, String legislation2, String data1, String data2, String data3, String data4) {
+
+    }
+}
---
+++ src/main/java/me/rtx4090/api/TPH.java
@@ -0,0 +1,15 @@
+package me.rtx4090.reportWebsite;
+
+import org.openqa.selenium.WebDriver;
+
+public class TPH extends Catalog {
+    @Override
+    public void getElement() {
+
+    }
+
+    @Override
+    public void verify() {
+
+    }
+}
